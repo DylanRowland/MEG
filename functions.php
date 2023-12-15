ca1b0248f333c8143c090dab1d2c9ac2595f28b6
DylanRowland <119446681+DylanRowland@users.noreply.github.com>
2023-12-15 12:18:02 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,37 @@
-<?php session_start();
-
-  function nav() {
-    echo '<div class="navbar">';
-    echo '<a href="https://replit.com/@XAVIEROGLE/e#home.html">Home</a>';
-    echo '<a href="#">About</a>';
-    echo '<a href="#">Contact</a>';
-    echo '<a href="#">Services</a>';
-    echo '<a href="#">Portfolio</a>';
-    echo '</div>';
-  }
+<?php require($_SERVER['DOCUMENT_ROOT'].'/functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
 
 
-?>
+</head>
+
+
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multi Ending Game</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+  <header>
+      <h1>Multi Ending Game</h1>
+      <!-- Add navigation or any header content here -->
+  </header>
+
+  <body>
+      <div class="landingNav">
+        <a href="about.php">Start</a>
+        <br>
+        <a href="contact.php">Profile</a>
+        <br>
+        <a href="contact.php">Log Out</a>
+        <br>
+        <a href="template.php">Template</a>
+      </div>
+  </body>
+
+  <footer>
+
+  </footer>
+<link rel="stylesheet" type="text/css" href="style.css">
+</html>
