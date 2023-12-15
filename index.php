e754c461d5c510179fa91530ae7c1a2df704ae1f
DylanRowland <119446681+DylanRowland@users.noreply.github.com>
2023-12-15 12:12:54 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php require($_SERVER['DOCUMENT_ROOT'].'/functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+
+
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multi Ending Game</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+  <header>
+      <h1>Multi Ending Game</h1>
+      <!-- Add navigation or any header content here -->
+  </header>
+
+  <body>
+      <div class="landingNav">
+        <a href="index.php">Home</a>
+        <br>
+        <a href="about.php">About</a>
+        <br>
+        <a href="contact.php">Contact</a>
+      </div>
+      </div>
+  </body>
+  </body>
+
+  <footer>
+
+  </footer>
+<link rel="stylesheet" type="text/css" href="style.css">
+</html>
